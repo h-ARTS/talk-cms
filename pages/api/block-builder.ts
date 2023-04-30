680848f90fb1ba986f0aa9b90a9b0b25c9d48d3c
Hanan Mufti <hi5hanan@yahoo.com>
2023-04-30 23:29:20 +0200

    add openai package

--- pages/api/block-builder.ts
+++ pages/api/block-builder.ts
@@ -1,5 +1,12 @@
 import axios from "axios"
 import { NextApiRequest, NextApiResponse } from "next"
+import { Configuration, OpenAIApi } from "openai"
+
+const configuration = new Configuration({
+  organization: process.env.OPENAI_ORG_ID,
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
 
 type JsonResponse = Array<{
   type: string
@@ -16,32 +23,31 @@ export default async function handler(
   const userInput = req.body.input
 
   try {
-    const openaiResponse = await axios.post(
-      "https://api.openai.com/v1/engines/davinci-codex/completions",
-      {
-        prompt: `Given the user input "${userInput}", generate a JSON schema for a landing page using only the supported elements [${supportedElements.join(
-          ", "
-        )}] and in the following format: [{ type: 'element_type', children: [ { type: 'child_element_type', content: { key: 'value' } } ], content: { key: 'value' } }]`,
-        max_tokens: 150,
-        n: 1,
-        stop: null,
-        temperature: 0.5,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    const openaiResponse = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Given the user input "${userInput}", generate a JSON schema for a landing page using only the supported elements [${supportedElements.join(
+            ", "
+          )}] and in the following format: [{ type: 'element_type', children: [ { type: 'child_element_type', content: { key: 'value' } } ], content: { key: 'value' } }]`,
         },
-      }
-    )
+      ],
+      max_tokens: 500,
+      n: 1,
+      temperature: 1,
+    })
+    console.log(openaiResponse)
 
     const processedJson: JsonResponse = processOpenAIResponse(
-      openaiResponse.data.choices[0].text
+      openaiResponse.data.choices[0].message?.content!
     )
+    console.log(processedJson)
     const validationResult = validateJsonSchema(
       processedJson,
       supportedElements
     )
+    console.log(validationResult)
 
     if (validationResult.valid) {
       res.status(200).json(processedJson)
